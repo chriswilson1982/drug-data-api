607c300e25ff7e6a3fc70f3fb4bb44efbb31ee0c
Chris Wilson <c.m.wilson@me.com>
2022-08-24 23:50:28 +0100

    General updates. Added FDA endpoint.

--- ampoule-api/main.py
+++ ampoule-api/main.py
@@ -1,28 +1,48 @@
 """Ampoule drug data API: Get drug data for a submitted Global Trade Identification Number.
 
-Submit a GET request to '/gtin/<GTIN>', where <GTIN> is a 13 or 14 digit Global Trade Identification Number. This API queries a MongoDB database and uses modified data from the Dictionary of Medicines and Devices (published by NHS Digital and available under an Open Government licence) to link GTIN to Actual Medicinal Product Pack (AMPP) data. The API returns a JSON object including the name, strength, units, type and quanity of the product, if available.
+UK: Submit a GET request to '/api/dmd/gtin/<GTIN>', where <GTIN> is a 13 or 14 digit Global Trade Identification Number (GTIN). 
+
+USA: Submit a GET request to '/api/fda/ndc/<NDC>', where <NDC> is a dehyphenated National Drug Code (NDC). 
+
+This API queries a MongoDB database that uses modified data from the Dictionary of Medicines and Devices
+(published by NHS Digital and available under an Open Government licence) and the Food and Drug Administration (FDA)
+to serve drug data on submission of a valid GTIN or NDC.
+
+The API returns a JSON object including the name, strength, units, type and quanity of the product, if available.
 
 """
 
+
 import os
 import pymongo
-from bottle import Bottle, install, route, request, get, post, template, redirect, static_file, error, run
+from bottle import Bottle, install, route, request, get, post, template, redirect, response, static_file, error, run
 from bson.json_util import dumps
 import json
 import dns
+from datetime import datetime
+import hashlib
+import markdown
+
 
 MONGODB_URI = "mongodb+srv://..."
+
 
 # MongoDB connection
 mongo = pymongo.MongoClient(
     MONGODB_URI, maxPoolSize=50, connect=False)
 db = pymongo.database.Database(mongo, 'ampoule')
+
+
+# Collections
 dmd_collection = pymongo.collection.Collection(db, 'dmd')
+fda_collection = pymongo.collection.Collection(db, 'fda')
+
 
 # Create Bottle app instance
 app = Bottle()
 
 
+# Convenience function for successful response
 def make_response(data, message, status="success", error=False):
     """Return a standardised response object."""
     return {
@@ -33,13 +53,21 @@ def make_response(data, message, status="success", error=False):
     }
 
 
+# Convenience function for error response
 def make_error(message):
     """Return error response."""
     return make_response(None, message, status="fail", error=True)
 
 
-@app.get('/gtin/<gtin>')
-def get_drug_data(gtin):
+# Static javascript
+@app.get('/js/<filename>')
+def js(filename):
+    return static_file(filename, root='./static/js/', mimetype='text/javascript')
+
+
+# New API endpoint - DM+D (UK)
+@app.get('/api/dmd/gtin/<gtin>')
+def dmd_api(gtin):
     """Accept GTIN and return corresponding drug data."""
     # Check valid input
     if not gtin.isnumeric():
@@ -81,12 +109,64 @@ def get_drug_data(gtin):
         return make_error("Error: No drug data found for that GTIN")
 
 
+
+# New API endpoint - FDA (USA)
+@app.get('/api/fda/ndc/<ndc>')
+def fda_api(ndc):
+    def insertZero(str, pos):
+        return str[:pos-1] + "0" + str[pos-1:]
+    # 10-digit NDC - if unsuccessful try inserting zero in first or sixth position
+    # Check valid input
+    if not ndc.isnumeric():
+        return make_error("Error: Submit a 10 or 11 digit numeric NDC")
+    elif len(ndc) != 10 and len(ndc) != 11:
+        return make_error("Error: Submit a 10 or 11 digit numeric NDC")
+    # Make database request
+    try:
+        query = fda_collection.find_one({"package_ndc": ndc}, {"base_ndc": 0,"package_ndc": 0, "_id": 0})
+        result = json.loads(dumps(query))
+    except Exception as error:
+        return make_error(f"Error: {str(error)}")
+    # If no result, check equivalant GTIN
+    if not result:
+        # If 13-digit GTIN, try prefixing "0"
+        if len(ndc) == 10:
+            new_ndc = insertZero(ndc, 0)
+            try:
+                query = fda_collection.find_one({"package_ndc": new_ndc}, {"base_ndc": 0,"package_ndc": 0, "_id": 0})
+                result = json.loads(dumps(query))
+            except Exception as error:
+                return make_error(f"Error: {str(error)}")
+            if not result:
+                try:
+                    new_ndc = insertZero(ndc, 6)
+                    query = fda_collection.find_one({"package_ndc": new_ndc}, {"base_ndc": 0,"package_ndc": 0, "_id": 0})
+                    result = json.loads(dumps(query))
+                except Exception as error:
+                    return make_error(f"Error: {str(error)}")
+        # If 14-digit GTIN, try without first digit
+    elif len(ndc) == 11 and ndc[0] == "0":
+            new_ndc = ndc[1:]
+            try:
+                query = fda_collection.find_one({"package_ndc": new_ndc}, {"base_ndc": 0,"package_ndc": 0, "_id": 0})
+                result = json.loads(dumps(query))
+            except Exception as error:
+                return make_error(f"Error: {str(error)}")
+    # Return data
+    if result:
+        return make_response(result, "Success")
+    else:
+        return make_error("Error: No drug data found for that GTIN")
+
+
+@app.get('/test')
+def api_test():
+    return make_response(None, "API Status OK")
+
 @app.get('/')
-@app.get('/gtin')
-@app.get('/gtin/')
 def handle_root_url():
     """Return information response for root URL."""
-    return make_response(None, r"Ampoule drug data API. Submit a GET request to '/gtin/<GTIN>', where <GTIN> is a 13 or 14 digit Global Trade Identification Number. This API uses data from the Dictionary of Medicines and Devices (published by NHS Digital and available under an Open Government licence) to link GTIN to Actual Medicinal Product Pack (AMPP) data. Returns JSON object including name, strength, units, type and quanity of the product, if available.")
+    return make_response(None, r"Ampoule drug data API. Submit a GET request to '/api/dmd/gtin/<GTIN> or '/api/fda/ndc/<NDC>', where <GTIN> is a Global Trade Identification Number and NDC is a National Drug Code. This API uses adapted data from the Dictionary of Medicines and Devices (published by NHS Digital and available under an Open Government licence) and data from the Food and Drug Administration (FDA).  Returns a JSON object including the name, strength, units, type and quanity of the drug, if available.")
 
 
 @error(404)
